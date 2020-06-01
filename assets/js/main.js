b3dee0cbfb780890e0f9aacae9f7c1c8721a015c
Hao-Ning Wu <hnw244@nyu.edu>
2020-06-01 15:43:23 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -418,7 +418,7 @@ jQuery(document).ready(function($) {
     /* -------------------------------- skillbar ---------------------------- */
     /* ---------------------------------------------------------------------- */
 
-    $('.tabs-resume').on("click", function() {
+    $('.tabs-resume').on("click touchstart ", function() {
 
         $('.skillbar').each(function() {
             $(this).find('.skillbar-bar').width(0);
